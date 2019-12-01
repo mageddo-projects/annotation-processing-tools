4f057a3c18343fb3b4c026bb2a7cf841beff8c01
Elvis de Freitas <edigitalb@gmail.com>
2019-12-01 18:39:00 -0300

    codestyle

--- aptools/src/main/java/com/mageddo/aptools/elements/ElementFinder.java
+++ aptools/src/main/java/com/mageddo/aptools/elements/ElementFinder.java
@@ -8,24 +8,28 @@ import javax.lang.model.element.ElementKind;
 
 public class ElementFinder {
 
-	private ElementFinder() {
-	}
+  private ElementFinder() {
+  }
 
-	/**
-	 * Find all enclosing elements which match #kind
-	 */
-	public static List<Element> find(Element e, ElementKind kind){
-		final List<Element> elements = new ArrayList<>();
-		find(e, kind, elements);
-		return elements;
-	}
+  /**
+   * Find all enclosing elements which match #kind
+   */
+  public static List<Element> find(Element e, ElementKind kind) {
+    final List<Element> elements = new ArrayList<>();
+    find(e, kind, elements);
+    return elements;
+  }
 
-	private static void find(Element e, ElementKind kind, List<Element> elements) {
-		for (final Element enclosedElement : e.getEnclosedElements()) {
-			if(enclosedElement.getKind().equals(kind)){
-				elements.add(enclosedElement);
-			}
-			find(enclosedElement, kind, elements);
-		}
-	}
+  private static void find(Element e, ElementKind kind, List<Element> elements) {
+    for (final Element enclosedElement : e.getEnclosedElements()) {
+      if (enclosedElement.getKind().equals(kind)) {
+        elements.add(enclosedElement);
+      }
+      find(enclosedElement, kind, elements);
+    }
+  }
+
+  interface Predicate {
+	  
+  }
 }

6897b1d5987fab4a4d5a47681bcc35500423394c
Elvis de Freitas <edigitalb@gmail.com>
2019-12-01 23:30:15 -0300

    some tries

--- lombok-ext/src/main/java/com/mageddo/aptools/textblock/TextBlockProcessor.java
+++ lombok-ext/src/main/java/com/mageddo/aptools/textblock/TextBlockProcessor.java
@@ -2,6 +2,7 @@ package com.mageddo.aptools.textblock;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -9,6 +10,7 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
@@ -26,10 +28,11 @@ import lombok.TextBlock;
 public class TextBlockProcessor implements Processor {
   private final Logger logger = LoggerFactory.getLogger();
   private final ProcessingEnvironment processingEnv;
-  private Element lastClass;
+  private Set<Element> classes;
 
   public TextBlockProcessor(ProcessingEnvironment processingEnv) {
     this.processingEnv = processingEnv;
+    this.classes = new HashSet<>();
   }
 
   @Override
@@ -37,7 +40,7 @@ public class TextBlockProcessor implements Processor {
     try {
     logger.warn("processingover=%s, tmp: %s", roundEnv.processingOver(), roundEnv.getRootElements());
     for (final Element element : roundEnv.getRootElements()) {
-      this.lastClass = element;
+      this.classes.add(element);
       final List<Element> textBlockVar = ElementFinder.find(element,
           textBlockAnnotatedElementPredicate()
       );
@@ -51,15 +54,64 @@ public class TextBlockProcessor implements Processor {
       logger.warn("class=%s, textBlockVar=%s, children=%s", element, textBlockVar, childs);
 
     }
+
+      for (Element aClass : classes) {
+        if(aClass.toString().contains("Fruit")){
+          System.out.println("deleting Fruit class");
+          Symbol.ClassSymbol classSymbol = (Symbol.ClassSymbol) aClass;
+//          classSymbol.classfile = classSymbol.sourcefile;
+
+          JavaFileObject fruitB = classSymbol.sourcefile;
+//          JavaFileObject fruitB = processingEnv
+//              .getFiler()
+//              .createSourceFile("Fruit", aClass);
+          System.out.println("=======");
+          Writer writer = fruitB.openWriter();
+          writer.write("package com.mageddo.lombok; public class Fruit {}");
+          writer.close();
+          classSymbol.sourcefile = fruitB;
+//      System.out.printf("writer=%s, javaFileObject=%s, %n", writer, fruitB);
+//          System.out.printf("writer=%s, %n", classSymbol);
+        }
+      }
+
     if(roundEnv.processingOver()){
-      logger.warn("writing to class " + this.lastClass);
-      Symbol.ClassSymbol lastClassUnit = (Symbol.ClassSymbol) this.lastClass;
-      CompilationUnit cu = JavaParser.parse(lastClassUnit.sourcefile.openReader(true), true);
-//      System.out.println("classfile: " + lastClassUnit.sourcefile);
-      System.out.println(cu.toString());
-      Writer writer = lastClassUnit.sourcefile.openWriter();
-      writer.write(cu.toString());
-      writer.close();
+      logger.warn("writing to class " + this.classes);
+//      Symbol.ClassSymbol lastClassUnit = (Symbol.ClassSymbol) this.classes;
+//      CompilationUnit cu = JavaParser.parse(lastClassUnit.sourcefile.openReader(true), true);
+//      System.out.println("classfile: " + lastClassUnit.classfile);
+//      System.out.println(cu.toString());
+
+//      for (Element aClass : classes) {
+//        if(aClass.toString().contains("Fruit")){
+//          System.out.println("deleting Fruit class");
+//          Symbol.ClassSymbol classSymbol = (Symbol.ClassSymbol) aClass;
+////          classSymbol.classfile = classSymbol.sourcefile;
+//
+//          JavaFileObject fruitB = processingEnv
+//          .getFiler()
+//          .createSourceFile("Fruit", aClass);
+//          System.out.println("=======");
+//          Writer writer = fruitB.openWriter();
+//          writer.write("package com.mageddo.lombok; public class Fruit {}");
+//          writer.close();
+//          classSymbol.sourcefile = fruitB;
+////      System.out.printf("writer=%s, javaFileObject=%s, %n", writer, fruitB);
+////          System.out.printf("writer=%s, %n", classSymbol);
+//        }
+//      }
+//      JavaFileObject fruitB = processingEnv
+//          .getFiler()
+//          .createSourceFile("Fruit");
+//      System.out.println("=======");
+//      Writer writer = fruitB.openWriter();
+//      System.out.printf("writer=%s, javaFileObject=%s, %n", writer, fruitB);
+//      writer.write("package com.mageddo.lombok; public class Fruit {}");
+//      writer.close();
+
+//      Writer writer = lastClassUnit.sourcefile.openWriter();
+//      writer.write(cu.toString());
+//      writer.close();
     }
 
     }catch (Exception e){

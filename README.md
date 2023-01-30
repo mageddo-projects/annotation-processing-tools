5c82b67c7c6adc6153e0fa86e28159d7a8b32fc5
Elvis de Freitas <edigitalb@gmail.com>
2023-01-30 00:09:38 -0300

    Clearing code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# annotation-processing-tools
+## annotation-processing-tools
 A set of useful tools when creating annotation processing libs
+
+## [Lombok-Ext][1]
+* Adds support to Text Blocks in Java 7+
+
+## [Reflection Config Generator][2]
+
+[1]: lombok-ext
+[1]: reflection-config-generator
+
+## Require
--- reflection-config-generator/src/main/java/nativeimage/core/thirdparty/ThirdPartyPackageScanner.java
+++ reflection-config-generator/src/main/java/nativeimage/core/thirdparty/ThirdPartyPackageScanner.java
@@ -1,19 +1,14 @@
 package nativeimage.core.thirdparty;
 
-import java.lang.reflect.Modifier;
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.mageddo.aptools.ClassUtils;
 import com.mageddo.aptools.log.Logger;
-
 import com.mageddo.aptools.log.LoggerFactory;
 
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
 
 public class ThirdPartyPackageScanner {
 
@@ -26,23 +21,19 @@ public class ThirdPartyPackageScanner {
 	 */
 	public static Set<String> findPackageClasses(String packageName) {
 
-//		final Set<Class<?>> classes = new Reflections(new ConfigurationBuilder()
-//				.setScanners(Scanners.SubTypes.filterResultsBy(s -> true),
-//						Scanners.Resources
-//				)
-//				.addUrls(ClasspathHelper.forJavaClassPath())
-//				.filterInputsBy(new FilterBuilder()
-//						.includePackage(packageName)))
-//				.getSubTypesOf(Object.class);
-
 		final Set<String> classes = new Reflections(packageName)
 				.getAll(Scanners.SubTypes);
 
-
-		final Set<String> filteredClasses = classes
-				.stream()
-				.filter(it -> ClassUtils.doPackageOwnClass(packageName, it))
-				.collect(Collectors.toSet());
+		final Set<String> filteredClasses = new LinkedHashSet<>();
+		for (String clazz : classes) {
+			if (ClassUtils.doPackageOwnClass(packageName, clazz)) {
+				filteredClasses.add(clazz);
+			}
+		}
+//		final Set<String> filteredClasses = classes
+//				.stream()
+//				.filter(it -> ClassUtils.doPackageOwnClass(packageName, it))
+//				.collect(Collectors.toSet());
 
 		log.warn(
 				"status=packageScanned, classes=%d, afterFilter=%d, package=%s",
@@ -51,26 +42,4 @@ public class ThirdPartyPackageScanner {
 		return filteredClasses;
 	}
 
-	public static void main(String[] args) {
-		final Set<String> classes = new Reflections("com.fasterxml.jackson.core.util")
-				.getAll(Scanners.SubTypes);
-
-		System.out.println(classes);
-
-
-		final Reflections reflections = new Reflections(new ConfigurationBuilder()
-				.setScanners(Scanners.SubTypes.filterResultsBy(s -> true),
-						Scanners.Resources
-				)
-				.addUrls(ClasspathHelper.forJavaClassPath())
-				.filterInputsBy(new FilterBuilder()
-						.includePackage("com.fasterxml.jackson.core.util")));
-
-		System.out.println(reflections.getSubTypesOf(Object.class));
-		;
-	}
-
-	static boolean isValidClass(Class<?> clazz) {
-		return !clazz.isInterface() && !Modifier.isAbstract(clazz.getModifiers());
-	}
 }
